d636a09e038094f72e733ef1db0ccd82a535d3ea
mario.fusco <mario.fusco@a7b4fb0e-dcbf-11dd-bad4-6dbcaf1fdc36>
2009-01-08 16:26:15 +0000

    group refactor

--- src/test/java/ch/lambdaj/GroupByTest.java
+++ src/test/java/ch/lambdaj/GroupByTest.java
@@ -1,7 +1,8 @@
 package ch.lambdaj;
 
-import static ch.lambdaj.Groups.*;
+import static ch.lambdaj.group.Groups.*;
 import static ch.lambdaj.Lambda.*;
+import static ch.lambdaj.TestUtil.*;
 import static ch.lambdaj.mock.ExposureBy.*;
 import static java.util.Arrays.*;
 import static org.hamcrest.Matchers.*;
@@ -9,18 +10,11 @@ import static org.junit.Assert.*;
 
 import java.util.*;
 
-import net.sf.json.*;
-import net.sf.json.util.*;
-
 import org.jmock.*;
 import org.junit.*;
 
 import ch.lambdaj.mock.*;
 
-/**
- * @author Mario Fusco
- * @author Luca Marrocco
- */
 public class GroupByTest {
 	private Mockery context = new Mockery();
 
@@ -30,6 +24,8 @@ public class GroupByTest {
 			{
 				setCountryName("France");
 				setInsuredName("Fex France");
+				setCountryIso("FR");
+				setCountryFlag("/flags/fr.jpg");
 			}
 		};
 
@@ -37,6 +33,8 @@ public class GroupByTest {
 			{
 				setCountryName("Canada");
 				setInsuredName("Fex Canada");
+				setCountryIso("CA");
+				setCountryFlag("/flags/ca.jpg");
 			}
 		};
 
@@ -49,7 +47,7 @@ public class GroupByTest {
 
 	@Test
 	public void testGroupByInsuredName() {
-		String string = string(group(exposures, by(Exposure).insuredName()));
+		String string = toJsonString(group(exposures, by(Exposure).insuredName()));
 
 		assertThat(string, containsString("\"insuredName\":\"Fex France"));
 		assertThat(string, containsString("\"insuredName\":\"Fex Canada"));
@@ -57,7 +55,7 @@ public class GroupByTest {
 
 	@Test
 	public void testGroupByInsuredNameAndCountryName() {
-		String string = string(group(exposures, by(Exposure).insuredName().countryName()));
+		String string = toJsonString(group(exposures, by(Exposure).insuredName().countryName()));
 		assertThat(string, containsString("\"insuredName\":\"Fex France\",\"children\":"));
 		assertThat(string, containsString("\"countryName\":\"France\",\"children\":"));
 		assertThat(string, containsString("\"insuredName\":\"Fex Canada\",\"children\":"));
@@ -66,32 +64,36 @@ public class GroupByTest {
 
 	@Test
 	public void testGroupByInsuredNameAsExposures() {
-		String string = string(group(exposures, by(Exposure).insuredName().asExposures()));
+		String string = toJsonString(group(exposures, by(Exposure).insuredName().asExposures()));
 	}
 
 	@Test
 	public void testGroupByCountryName() {
-		String string = string(group(exposures, by(Exposure).countryName()));
+		String string = toJsonString(group(exposures, by(Exposure).countryName().asInsureds().headCountryIso()));
+		System.out.println(string);
+		
+		string = cleanJsonString(string);
+		assertThat(string, containsString("insureds:[{countryName:France,insuredName:Fex France,countryFlag:/flags/fr.jpg,countryIso:FR}]"));
+		assertThat(string, containsString("insureds:[{countryName:Canada,insuredName:Fex Canada,countryFlag:/flags/ca.jpg,countryIso:CA}]"));
+		
+		string = cleanJsonString(string, cleanJsonString(toJsonString(FexFrance)));
+		string = cleanJsonString(string, cleanJsonString(toJsonString(FexCanada)));
+		assertThat(string, containsString("countryIso:FR"));
+		assertThat(string, containsString("countryIso:CA"));
 	}
 
 	@Test
 	public void testGroupByCountryNameAndInsuredName() {
-		String string = string(group(exposures, by(Exposure).countryName().insuredName()));
+		String string = toJsonString(group(exposures, by(Exposure).countryName().insuredName()));
 	}
 
 	@Test
 	public void testGroupByCountryNameAsCountries() {
-		String string = string(group(exposures, by(Exposure).countryName().asCountries()));
+		String string = toJsonString(group(exposures, by(Exposure).countryName().asCountries()));
 	}
 
 	@Test
 	public void testGroupByCountryNameAsExposures() {
-		String string = string(group(exposures, by(Exposure).countryName().asExposures()));
-	}
-
-	private String string(Object object) {
-		if (JSONUtils.isArray(object)) return JSONArray.fromObject(object).toString();
-		if (JSONUtils.isObject(object)) return JSONObject.fromObject(object).toString();
-		return object.toString();
+		String string = toJsonString(group(exposures, by(Exposure).countryName().asExposures()));
 	}
 }
