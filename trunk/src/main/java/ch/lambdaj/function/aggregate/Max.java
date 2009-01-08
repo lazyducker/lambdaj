5cbef4f3ca4757d3275863c05d59dc868b628e00
luca.marrocco <luca.marrocco@a7b4fb0e-dcbf-11dd-bad4-6dbcaf1fdc36>
2009-01-08 17:16:15 +0000

    adding license

--- trunk/src/main/java/ch/lambdaj/function/aggregate/Max.java
+++ trunk/src/main/java/ch/lambdaj/function/aggregate/Max.java
@@ -1,3 +1,8 @@
+/**
+ * // Modified or written by Ex Machina SAGL for inclusion with lambdaj.
+ * // Copyright (c) 2008 Mario Fusco, Luca Marrocco.
+ * // Licensed under the Apache License, Version 2.0 (the "License")
+ */
 package ch.lambdaj.function.aggregate;
 
 
