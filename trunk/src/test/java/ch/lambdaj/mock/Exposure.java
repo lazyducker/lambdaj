87b46ae54ed0dba1d82233053af080f4e6e0024b
mario.fusco <mario.fusco@a7b4fb0e-dcbf-11dd-bad4-6dbcaf1fdc36>
2009-01-08 16:26:15 +0000

    group refactor

--- trunk/src/test/java/ch/lambdaj/mock/Exposure.java
+++ trunk/src/test/java/ch/lambdaj/mock/Exposure.java
@@ -19,11 +19,29 @@ public class Exposure {
 	public void setInsuredName(String insuredName) {
 		this.insuredName = insuredName;
 	}
+	
+	public String getCountryIso() {
+		return countryIso;
+	}
+	public void setCountryIso(String countryIso) {
+		this.countryIso = countryIso;
+	}
+	
+	public String getCountryFlag() {
+		return countryFlag;
+	}
+	public void setCountryFlag(String countryFlag) {
+		this.countryFlag = countryFlag;
+	}
 
 	private String countryName;
 
 	private String insuredName;
 
+	private String countryIso;
+	
+	private String countryFlag;
+	
 	public Exposure() {}
 	
 	public Exposure(String countryName, String insuredName) {
@@ -33,6 +51,6 @@ public class Exposure {
 	
 	@Override
 	public String toString() {
-		return join(forEach(asList(countryName, insuredName)));
+		return join(from(asList(countryName, insuredName)));
 	}
 }
