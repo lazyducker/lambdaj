0763205eb7957f0afe6c4f69c37ea1390ecae425
mario.fusco <mario.fusco@a7b4fb0e-dcbf-11dd-bad4-6dbcaf1fdc36>
2009-01-30 13:47:04 +0000

    collect methods signature fixed

--- src/main/java/ch/lambdaj/Lambda.java
+++ src/main/java/ch/lambdaj/Lambda.java
@@ -90,7 +90,7 @@ public class Lambda {
 	// /// Collection
 	// ////////////////////////////////////////////////////////////////////////
 
-	public static <T> List<T> collect(Object iterable) {
+	public static <T> List<? extends T> collect(Object iterable) {
 		List<T> collected = new LinkedList<T>();
 		for (Object item : (Iterable<?>) iterable) {
 			if (item instanceof Iterable) collected.addAll((Collection<T>) collect(item));
@@ -101,7 +101,7 @@ public class Lambda {
 	}
 	
 	public static <T> List<T> collect(Object iterable, T argument) {
-		return collect(convert(iterable, new ArgumentConverter<Object, Object>(argument)));
+		return (List<T>)collect(convert(iterable, new ArgumentConverter<Object, Object>(argument)));
 	}
 
 	// ////////////////////////////////////////////////////////////////////////
