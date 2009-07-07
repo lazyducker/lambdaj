e90ccb64b993ee3dfedee37ff4eb103ae157d998
Christian.Raschka <Christian.Raschka@a7b4fb0e-dcbf-11dd-bad4-6dbcaf1fdc36>
2009-07-07 09:30:37 +0000

    [maven-release-plugin]  copy for tag lambdaj-1.14

---
+++ tags/lambdaj-1.14/src/main/java/ch/lambdaj/function/matcher/HasArgumentWithValue.java
@@ -0,0 +1,53 @@
+package ch.lambdaj.function.matcher;
+
+import static ch.lambdaj.function.argument.ArgumentsFactory.*;
+
+import org.hamcrest.*;
+
+import ch.lambdaj.function.argument.*;
+
+public class HasArgumentWithValue<T, A> extends LambdaJMatcher<T> {
+	
+    private final Argument<A> argument;
+    private final Matcher<? extends Object> value;
+
+    public HasArgumentWithValue(Argument<A> argument, Matcher<? extends Object> value) {
+        this.argument = argument;
+        this.value = value;
+    }
+
+	public boolean matches(Object item) {
+		if (argument == null) return false;
+        try {
+            return value.matches(argument.evaluate(item));
+        } catch (Exception e) {
+            return false;
+        } 
+	}
+	
+    public void describeTo(Description description) {
+        description.appendText("hasArgument(");
+        description.appendValue(argument.getInkvokedPropertyName());
+        description.appendText(", ");
+        description.appendDescriptionOf(value);
+        description.appendText(")");
+    }
+
+    @Factory
+    public static <T> HasArgumentWithValue<T, Boolean> having(Boolean argument) {
+    	return having(argument, booleanMatcher);
+    }
+    
+    @Factory
+    public static <T, A> HasArgumentWithValue<T, A> having(A argument, Matcher<? extends Object> value) {
+    	return new HasArgumentWithValue<T, A>(actualArgument(argument), value);
+    }
+
+    private static BooleanMatcher booleanMatcher = new BooleanMatcher();
+    private static class BooleanMatcher extends BaseMatcher<Boolean> {
+		public boolean matches(Object item) {
+			return ((Boolean)item).booleanValue();
+		}
+		public void describeTo(Description description) { }
+    }
+}
