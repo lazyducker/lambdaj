ec34fef3cea4bd1503d0290dd56b143034ce2ca2
Christian.Raschka <Christian.Raschka@a7b4fb0e-dcbf-11dd-bad4-6dbcaf1fdc36>
2009-07-08 12:09:55 +0000

    [maven-release-plugin]  copy for tag lambdaj-1.14-r9

---
+++ tags/lambdaj-1.14-r9/src/main/java/ch/lambdaj/group/LeafGroup.java
@@ -0,0 +1,60 @@
+package ch.lambdaj.group;
+
+import java.util.*;
+
+public class LeafGroup<T> implements Group<T> {
+
+	private List<T> list;
+	private Map<String, Object> headMap = new TreeMap<String, Object>();
+	
+	@SuppressWarnings("unchecked")
+	LeafGroup(Map<String, Object> map, String childrenNodeName) {
+		headMap = new TreeMap<String, Object>(map);
+		list = (List<T>)headMap.remove(childrenNodeName);
+	}
+	
+	public List<T> find(String key) {
+		return list;
+	}
+
+	public List<T> find(Object key) {
+		return find(key.toString());
+	}
+	
+	public List<T> findAll() {
+		return list;
+	}
+
+	public Group<T> findGroup(String key) {
+		return this;
+	}
+	
+	public Group<T> findGroup(Object key) {
+		return findGroup(key.toString());
+	}
+	
+	public List<Group<T>> subgroups() {
+		return new ArrayList<Group<T>>();
+	}
+
+	public int getSize() {
+		return list.size();
+	}
+
+	public boolean isLeaf() {
+		return true;
+	}
+
+	public Set<String> keySet() {
+		return new HashSet<String>();
+	}
+
+	public String getHeadValue(String key) {
+		Object value = headMap.get(key);
+		return value == null ? "" : value.toString();
+	}
+
+	public Set<String> getHeads() {
+		return headMap.keySet();
+	}
+}
