25289780d727798ec10d986e4551cd5a58f16a12
Christian.Raschka <Christian.Raschka@a7b4fb0e-dcbf-11dd-bad4-6dbcaf1fdc36>
2009-07-08 13:37:58 +0000

    [maven-release-plugin]  copy for tag lambdaj-1.14-r17

---
+++ tags/lambdaj-1.14-r17/src/main/java/ch/lambdaj/function/matcher/HasNestedPropertyWithValue.java
@@ -0,0 +1,37 @@
+package ch.lambdaj.function.matcher;
+
+import static ch.lambdaj.util.IntrospectionUtil.*;
+
+import org.hamcrest.*;
+
+public class HasNestedPropertyWithValue<T> extends LambdaJMatcher<T> {
+
+    private final String propertyName;
+    private final Matcher<? extends Object> value;
+
+    public HasNestedPropertyWithValue(String propertyName, Matcher<? extends Object> value) {
+        this.propertyName = propertyName;
+        this.value = value;
+    }
+
+	public boolean matches(Object item) {
+        try {
+            return value.matches(getPropertyValue(item, propertyName));
+        } catch (Exception e) {
+            return false;
+        } 
+	}
+	
+    public void describeTo(Description description) {
+        description.appendText("hasProperty(");
+        description.appendValue(propertyName);
+        description.appendText(", ");
+        description.appendDescriptionOf(value);
+        description.appendText(")");
+    }
+
+    @Factory
+    public static <T> Matcher<T> hasNestedProperty(String propertyName, Matcher<? extends Object> value) {
+        return new HasNestedPropertyWithValue<T>(propertyName, value);
+    }
+}
