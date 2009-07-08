f55c47db17ba02a81aed50c6602c011a490cd34b
Christian.Raschka <Christian.Raschka@a7b4fb0e-dcbf-11dd-bad4-6dbcaf1fdc36>
2009-07-08 12:31:52 +0000

    [maven-release-plugin]  copy for tag lambdaj-1.14-r12

---
+++ tags/lambdaj-1.14-r12/src/main/java/ch/lambdaj/function/convert/StringLengthConverter.java
@@ -0,0 +1,9 @@
+package ch.lambdaj.function.convert;
+
+
+public class StringLengthConverter extends PropertyExtractor<String, Integer> {
+
+	public StringLengthConverter() {
+		super("length");
+	}
+}
