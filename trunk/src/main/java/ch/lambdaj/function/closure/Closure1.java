4699ed383fc80eca5e6b1397cb27f5903663b2f2
mario.fusco <mario.fusco@a7b4fb0e-dcbf-11dd-bad4-6dbcaf1fdc36>
2009-07-17 22:13:57 +0000

    curry

--- trunk/src/main/java/ch/lambdaj/function/closure/Closure1.java
+++ trunk/src/main/java/ch/lambdaj/function/closure/Closure1.java
@@ -15,4 +15,8 @@ public class Closure1<A> extends AbstractClosure {
 	public List<Object> each(Iterable<? extends A> params) {
 		return closeAll(params);
 	}
+	
+	public Closure0 curry(A curry) {
+		return curry(new Closure0(), curry, 1);
+	}
 }
--- trunk/src/main/java/ch/lambdaj/function/closure/Closure3.java
+++ trunk/src/main/java/ch/lambdaj/function/closure/Closure3.java
@@ -11,4 +11,16 @@ public class Closure3<A, B, C> extends AbstractClosure {
 	public List<Object> each(Iterable<? extends A> params1, Iterable<? extends B> params2, Iterable<? extends C> params3) {
 		return closeAll(params1, params2, params3);
 	}
+	
+	public Closure2<B, C> curry1(A curry) {
+		return curry(new Closure2<B, C>(), curry, 1);
+	}
+	
+	public Closure2<A, C> curry2(B curry) {
+		return curry(new Closure2<A, C>(), curry, 2);
+	}
+	
+	public Closure2<A, B> curry3(C curry) {
+		return curry(new Closure2<A, B>(), curry, 3);
+	}
 }
