d021f01992a9adbffa7b68ab5152295702564b08
mario.fusco <mario.fusco@a7b4fb0e-dcbf-11dd-bad4-6dbcaf1fdc36>
2010-10-10 21:25:23 +0000

    changing copyright message

--- trunk/src/main/java/ch/lambdaj/function/compare/PropertyComparator.java
+++ trunk/src/main/java/ch/lambdaj/function/compare/PropertyComparator.java
@@ -1,5 +1,5 @@
-// Modified or written by Ex Machina SAGL for inclusion with lambdaj.
-// Copyright (c) 2009 Mario Fusco.
+// Modified or written by Lambdascale SRL for inclusion with lambdaj.
+// Copyright (c) 2009-2010 Mario Fusco.
 // Licensed under the Apache License, Version 2.0 (the "License")
 
 package ch.lambdaj.function.compare;
